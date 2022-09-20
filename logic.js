810c0a270e7910c81ca72e14e070f797fd374cf4
Josue <josue.naure.c@gmail.com>
2022-09-20 01:14:21 -0500

    Solucione el problema del localStorage

--- logic.js
+++ logic.js
@@ -1,46 +1,56 @@
-import { personal, dias, meses, startTime, theDate } from "./utils.js"
+import { personal, dias, meses, startTime, theDate } from "./utils.js";
 
 // window.addEventListener("load", function(event){
 
 // })
-startTime()
-theDate()
+startTime();
+theDate();
 
-const headerFecha = document.querySelector('.header-fecha')
-const headerHora = document.querySelector('.header-hora')
+const headerFecha = document.querySelector(".header-fecha");
+const headerHora = document.querySelector(".header-hora");
 
-console.dir(headerFecha)
-const controlFormDni = document.querySelector('.control-form-dni')
-const controlFormNombres = document.querySelector('.control-form-nombres')
-const controlFormApellidos = document.querySelector('.control-form-apellidos')
-const controlFormTipo = document.querySelector('.control-form-tipo')
-const controlFormSubmit = document.querySelector('.control-form-submit')
-const viewanswer = document.querySelector('.view-answer')
+const controlFormDni = document.querySelector(".control-form-dni");
+const controlFormNombres = document.querySelector(".control-form-nombres");
+const controlFormApellidos = document.querySelector(".control-form-apellidos");
+const controlFormTipo = document.querySelector(".control-form-tipo");
+const controlFormSubmit = document.querySelector(".control-form-submit");
+const viewanswer = document.querySelector(".view-answer");
 
-controlFormDni.addEventListener('change', completeForm)
-controlFormSubmit.addEventListener('click', ()=>{
-viewanswer.innerHTML=`Dni: ${controlFormDni.value}, Nombres: ${controlFormNombres.value}, Apellidos: ${controlFormApellidos.value}, Tipo: ${controlFormTipo.value}, Fecha: ${headerFecha.value}, Hora: ${headerHora.value}`;
-})
-
+controlFormDni.addEventListener("change", completeForm);
+controlFormSubmit.addEventListener("click", guardar);
 
 function completeForm(e) {
-    console.log(e.target.value)
-    for (let i = 0; i < personal.length; i++) {
-        if (e.target.value == personal[i].dni) {
-            controlFormNombres.value = personal[i].nombres;
-            controlFormApellidos.value = personal[i].apellidos;
-        }
-        if (e.target.value == "") {
-            controlFormNombres.value = "";
-            controlFormApellidos.value = "";
-        }
+  console.log(e.target.value);
+  for (let i = 0; i < personal.length; i++) {
+    if (e.target.value == personal[i].dni) {
+      controlFormNombres.value = personal[i].nombres;
+      controlFormApellidos.value = personal[i].apellidos;
     }
+    if (e.target.value == "") {
+      controlFormNombres.value = "";
+      controlFormApellidos.value = "";
+    }
+  }
 }
 
+function guardar() {
+  const resp = {
+    dni: controlFormDni.value,
+    nombres: controlFormNombres.value,
+    apellidos: controlFormApellidos.value,
+    tipo: controlFormTipo.value,
+    fecha: headerFecha.innerText,
+    hora: headerHora.innerText,
+  };
 
+  const arrayRegistros = [];
+  if (!localStorage.getItem("a")) {
+    localStorage.setItem("a", JSON.stringify(resp));
+  } else {
+    arrayRegistros.push(JSON.parse(localStorage.getItem("a")));
+    arrayRegistros.push(resp);
+    localStorage.setItem("a", JSON.stringify(arrayRegistros.flat()));
+  }
+}
 
 //document.getElementById(dni).addEventListener(onselect,)
-
-
-
-
