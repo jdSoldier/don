a223d8be47bd267fe0906e631ad1b9eebe586869
Josue <josue.naure.c@gmail.com>
2022-09-21 12:22:35 -0500

    css v1

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-const { app, BrowserWindow} = require('electron')
+"use strict";
 
-const createWindow = () => {
-    const win = new BrowserWindow()
-    win.loadFile('index.html')
-}
+//instanciando los objetos app y BrowserWindow
+const { app, BrowserWindow } = require("electron");
 
-app.whenReady().then(()=>{createWindow()})
+//imprimiendo un mensaje en la consola antes de salir
+app.on("before-quit", () => {
+  console.log("saliendo");
+});
 
-console.log("hola");
+//ejecutando ordenes cuando la aplicacion esta lista
+app.on("ready", () => {
+  //creando una ventana
+  let win = new BrowserWindow({
+    show: false,
+    // width: 500,
+    // minWidth: 500,
+    // maxWidth: 500,
+    // height: 400,
+    // minHeight: 400,
+    // maxHeight: 400,
+    resizable: false,
+    title: "Hola Mundo!",
+    center: true,
+    maximizable: false,
+  });
+
+  win.once("ready-to-show", () => {
+    win.show();
+  });
+
+  //detectando la posicion del movimiento de la ventana
+  //   win.on("move", () => {
+  //     const position = win.getPosition();
+  //     console.log(`la posicion es ${position}`);
+  //   });
+
+  //detectando el cierre de la ventana para cerrar el aplicativo
+  win.on("closed", () => {
+    win = null;
+    app.quit();
+  });
+
+  win.loadURL(`file://${__dirname}/index.html`);
+
+  //   win.setMenu(null);
+});
--- utils.js
+++ utils.js
@@ -1,67 +1,70 @@
 const personal = [
-    {
-        dni: 75441002,
-        nombres: 'josue david',
-        apellidos: 'ñaure cervantes'
-    },
-    {
-        dni: 75441003,
-        nombres: 'pedro',
-        apellidos: 'pica piedra'
-    },
-
-]
+  {
+    dni: 75441002,
+    nombres: "josue david",
+    apellidos: "ñaure cervantes",
+  },
+  {
+    dni: 75441003,
+    nombres: "pedro",
+    apellidos: "pica piedra",
+  },
+];
 
 const dias = [
-    "Lunes",
-    "Martes",
-    "Miercoles",
-    "Jueves",
-    "Viernes",
-    "Sabado",
-    "Domingo"
-]
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves",
+  "Viernes",
+  "Sabado",
+  "Domingo",
+];
 
 const meses = [
-    "Enero",
-    "Febrero",
-    "Marzo",
-    "Abril",
-    "Mayo",
-    "Junio",
-    "Julio",
-    "Agosto",
-    "Setiembre",
-    "Octubre",
-    "Noviembre",
-    "Diciembre"
-]
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Setiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
 
 function startTime() {
-    var today = new Date()
-    var hr = today.getHours();
-    var min = today.getMinutes();
-    var sec = today.getSeconds();
-    var ap = (hr < 12)? "<span>AM</span>":"<span>PM</span>";
-    hr = (hr == 0)?12:hr;
-    hr = (hr > 12)?hr-12:hr;
-    hr = checkTime(hr);
-    min = checkTime(min);
-    sec = checkTime(sec);
-    document.getElementById("hora").innerHTML = `${hr} : ${min} : ${sec} : ${ap}`
-    var time = setTimeout(function(){startTime()},500)
+  var today = new Date();
+  var hr = today.getHours();
+  var min = today.getMinutes();
+  var sec = today.getSeconds();
+  var ap = hr < 12 ? "<span>AM</span>" : "<span>PM</span>";
+  hr = hr == 0 ? 12 : hr;
+  hr = hr > 12 ? hr - 12 : hr;
+  hr = checkTime(hr);
+  min = checkTime(min);
+  sec = checkTime(sec);
+  document.getElementById("hora").innerHTML = `${hr} : ${min} : ${sec} : ${ap}`;
+  var time = setTimeout(function () {
+    startTime();
+  }, 500);
 }
 
-function checkTime(i){
-    if (i < 10){
-        i="0"+i;
-    }
-    return i;
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
 }
 
-function theDate(){
-    let d=new Date()
-    document.getElementById("fecha").innerHTML = `${dias[d.getDay()]} ${d.getDate()} de ${meses[d.getMonth()]} del ${d.getFullYear()}`
+function theDate() {
+  let d = new Date();
+  document.getElementById("fecha").innerHTML = `${
+    dias[d.getDay() - 1]
+  } ${d.getDate()} de ${meses[d.getMonth()]} del ${d.getFullYear()}`;
 }
 
-export { personal, dias, meses, startTime, theDate}
+export { personal, dias, meses, startTime, theDate };
